78697f23bada0da19613be8cc89ac6fa9bc507b7
Jad Khalili <jad@videolab.ae>
2022-10-01 23:12:48 -0500

    Form changes

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,8 @@ function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [pageState, setPageState] = useState(1);
+  const [arrival, setArrival] = useState(null);
+  const [departure, setDeparture] = useState('LAX');
 
   // page state is working
   const incrementPageState = () => {
@@ -31,21 +33,54 @@ function App() {
     setPageState(pageState - 1);
   }
 
+  const onSetDeparture = (event) => {
+    console.log(event);
+    setDeparture(event.target.value);
+    console.log(departure);
+  }
+
+  const componentDidUpdate = (prevProps) => {
+    if(this.props.departure !== prevProps.departure) {
+      setDeparture(this.props.departure);
+    }
+  }
+
+  // const joinRoom = () => {
+  //   if (username !== "" && room !== "") {
+  //     socket.emit("join_room", room);
+  //     setShowChat(true);
+  //   }
+  // };
+
   return (
     <div className="App">
       { pageState === 1 ? (
         <div className="joinChatContainer">
           <h2>Tindair</h2> 
-          <button onClick={incrementPageState}>Get Started</button>
+          <div>
+             <img src={'./images/logo_wbg.png'} alt="this is logo image" />
+          </div>
+          <button class="button-main" onClick={incrementPageState}>Get Started</button>
         </div>
         ) : pageState === 2 ? (
           <div>
-          <h3>Select your airport</h3>
-          <select className="selector" autocomplete="on">
+          <h3>Select your departure airport</h3>
+          <select class="form-control" className="selector" value={departure} onInput={onSetDeparture}>
             <option value="IAH">IAH</option>
             <option value="LAX">LAX</option>
             <option value="EAS">EAS</option>
           </select>
+          <button class="button-main" onClick={incrementPageState}>Next</button>
+          </div>
+      ) : pageState == 3 ? (
+          <div>
+          <h3>Select your arrival airport</h3>
+          <select class="form-control" className="selector" autocomplete="on">
+            <option value="IAH">IAH</option>
+            <option value="LAX">LAX</option>
+            <option value="EAS">EAS</option>
+          </select>
+          <button class="button-main" onClick={incrementPageState}>Next</button>
         </div>
         // <Chat socket={socket} username={username} room={room} />
       ) : (<div> final div </div>)}
