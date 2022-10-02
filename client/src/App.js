9ec1bc2e961637083169c275422b8b41c507c355
HarshGG <ritzred2012@gmail.com>
2022-10-01 21:43:47 -0500

    Co-authored-by: Jad Khalili <Video-Lab@users.noreply.github.com> Co-authored-by: Casey <peicasey@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -8,39 +8,28 @@ const socket = io.connect("http://localhost:3001");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
+  const [pageState, setPageState] = useState(1);
 
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      setShowChat(true);
-    }
-  };
+  // page state is working
+  const incrementPageState = () => {
+    setPageState(pageState + 1);
+  }
+
+  const decrementPageState = () => {
+    setPageState(pageState - 1);
+  }
 
   return (
     <div className="App">
-      {!showChat ? (
+      { pageState === 1 ? (
         <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="John..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
+          <h3>Tindair</h3> 
+          <button onClick={incrementPageState}>Get Started</button>
         </div>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
+        ) : pageState === 2 ? (
+        <div>hey</div>
+        // <Chat socket={socket} username={username} room={room} />
+      ) : (<div> final div </div>)}
     </div>
   );
 }
